6ded1334a9a885e60ac26586a5b3b47121b41111
pcpercycruz <perscrcr@gmail.com>
2023-03-28 18:59:51 -0500

    nueva actu

---
+++ operadoresdecomparacion.js
@@ -0,0 +1,36 @@
+//operaciones de comparacion
+
+//console.log(4 < 7);
+//console.log(4 < 1);
+//console.log(4 > 4);
+//console.log(4 == 4);
+
+//Igualdad VS Igualdad stricta
+
+//console.log(3 == 3);
+//console.log(3 === 3);
+//console.log(3 === '3');
+//console.log(3 == '3');
+//console.log(7 == '7');
+//console.log(7 === '7');
+
+//Asignacion y asociatividad (precedencia es de mayor imnportancia se ejecuta)
+//en caso que sean del mismo valor de precedencia en el caso de == se usa la asociacion
+// para ejecutar segun el orden de la tabla en este caso de derecha a izquierda
+
+var a = 1;
+var b = 2;
+var c = a = b;
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+
+
+
+
+
+
+
+

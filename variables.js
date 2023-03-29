7532b2ce48625c76d60d175614af6b3d41519c89
pcpercycruz <perscrcr@gmail.com>
2023-03-28 19:01:46 -0500

    actu variables

---
+++ variables.js
@@ -0,0 +1,42 @@
+//variables string texto
+var bootcamp = 'henry';
+//variables numeros
+var numeroentero = 17;
+var numeronegativo = -4;
+var numerodecimal = 11.5;
+//booleanos verdadero o falso
+var estoyaprendiendo = true;
+var estoyaburrido = false;
+//undefined que no se asignado valor al recipiente o variable
+var cajon1;
+console.log(cajon1);
+//null  nulo o vacio a proposito
+var sinnada = null;
+
+
+//ejercicios con operadores con precedencia segun tabla de orden de mayor jerarquia
+
+//suma
+//console.log(3 + 5);
+//resta
+//consoles.log(14 - 7);
+//multiplicacion
+//console.log(8 * 6);
+//Division
+//console.log(30 / 9);
+//resto
+//console.log(95 % 4);
+//prueba de precedencia, aqui calcula rimero la multiplicacion luego suma y resta
+//console.log(3 + 5 * 2 - 8);
+//decirle que calcule primero la suma utilizar los parentesis de mas alta precedencia o importantes de ejecucion primera
+//console.log((3 + 5) * 2 - 8);
+//console.log('Hola' + 'Camilo');
+//var nombre = 'Camilo';
+//var saludo = 'Hola ' + nombre;
+//console.log(saludo);
+
+
+
+
+
+

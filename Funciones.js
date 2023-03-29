ff2ed2d7bf728f2bb739960b1b75d795899bc4ad
pcpercycruz <perscrcr@gmail.com>
2023-03-29 14:54:47 -0500

    actualizacion de funciones

---
+++ Funciones.js
@@ -0,0 +1,30 @@
+//Funcion = sumatres
+//primera opcion
+//function sumatres(x) {
+   //return x + 3;
+//}
+
+//Opcion2
+//var sumatres = function (x)  {
+    //return x + 3;
+//}
+
+//opcion3
+//var sumatres = (x) => {
+ // return x + 3  
+//}
+
+
+//funcion: cuidado con el consolelog
+//function cuidadoconelconsolelog(nombre) {
+    //console.log(nombre);
+    //return nombre;
+   // }
+
+   //precedencia de valores
+   var a = 1;
+   var b = 2;
+   var c = a = b;
+   console.log(c);
+   console.log(a);
+

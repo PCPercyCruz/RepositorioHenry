c9adda61f4cfaeb08478be05549dff9b50715559
pcpercycruz <perscrcr@gmail.com>
2023-03-29 14:54:07 -0500

    actualizacion de consiciones if

---
+++ controldeflujo.js
@@ -0,0 +1,30 @@
+//funcion condicional
+
+//function viajar(destino) {
+   // if (destino ==='Brasil'){
+        //console.log('Gire a la Izquierda');
+   // } else if (destino === 'Argentina'){
+       // console.log('Gire a la derecha');
+    //} else {
+       // console.log('Nos Pedimos');
+    //}
+//}
+
+//viajar('otro')
+
+//puede manejar
+
+function puedeManejar (edad) {
+    if(edad >=18){
+        console.log('Si Puede Manejar');
+    } else {
+        console.log('No Puede Manejra');
+    }
+}
+
+puedeManejar(18)
+
+
+
+
+

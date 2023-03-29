ef6b7bbedf589289e6dfde98a1696c0e33b6f002
pcpercycruz <perscrcr@gmail.com>
2023-03-29 16:04:20 -0500

    ObjetoMath.js

---
+++ ObjetoMath.js
@@ -0,0 +1,8 @@
+//objeto mach con sus metodos
+//Math.pow(2, 3);
+//Math.round(3.48);//devuelve 3 el mas proximo toamdo en cuanta q ue 3.50 ya es 4
+//Math.floor(5.96);//devuelve 5 entero menor proximo
+//Math.ceil(2.48);//devuelve entero 3 el mayor primoxo
+//Math.random()//devuel aleatorio decimal ente 0 y 1
+//Math.max(1, 2, 3, 4, 5);//devuelve valor maximo 5
+//Math.min(1, 2, 3, 4, 5)//devuelve valor minimo 1

36968170224b783b5ba4d7445b223e1d909a9231
pcpercycruz <perscrcr@gmail.com>
2023-04-16 11:43:48 -0500

    actu16-04-2023

---
+++ Prototipos.js
@@ -0,0 +1,37 @@
+//acceder al objeto array por medio del prototipo para crear propiedades y metodods y usarlos como herencia
+/* Array.prototype.mayorQueTres = function(){
+    var arregloModificado = [];
+    for(var i=0; i < this.length; i++){
+        if (this[i] > 3){
+            arregloModificado.push(false);
+        } else {
+            arregloModificado.push(this[i]);
+        }
+    }
+
+    return arregloModificado;
+}
+
+var areglo1 = [1,2,3,4,5];
+var nuevoarreglo = areglo1.mayorQueTres();
+
+console.log(nuevoarreglo); */
+
+//
+//
+//CREAR un metodo de  CLASE utilzando el proptipo
+
+class Latinoamerica {
+    constructor(){
+        this.paises = [];
+    };
+};
+
+Latinoamerica.prototype.agregarPais = function(pais){
+    this.paises.push(pais);
+    
+}
+
+var continente = new Latinoamerica();
+continente.agregarPais('Peru');
+console.log(continente);
---
+++ extension.js
@@ -0,0 +1,36 @@
+//Permite tener una clase general y heredadr propiedades y metodos a otras clases o subentidades que erender metodos 
+//y propiedad de la clase general y sus propios propiedades o metodos desubclases
+//Palabras CLAVE
+//EXTENDS //Permite extender una clase
+//SUPER //Permite indicar que propiedades se heredaran de la clase orincipal
+
+//Crear plase persona con su metodo saludar
+class persona {
+    constructor(nombre, edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    saludar(){
+        console.log('Hola, mi nombre es '+ this.nombre + '. tengo '+ this.edad + ' años.');
+    }
+}
+
+
+
+//crear otra clase con nombre programador y disignarla como extension de la clase principal persona
+
+class programador extends persona{
+    constructor(nombre, edad, añosDeExperiencia){
+        super(nombre, edad);
+        this.añosDeExperiencia = añosDeExperiencia;
+    }
+    codear(){
+        console.log('Soy '+this.nombre+'. codeo desde hace '+this.añosDeExperiencia+' años.')
+    }
+}
+
+let martin = new persona('Martin',25);
+let maria = new programador('María', 30,4);
+martin.saludar();
+maria.codear();
+
